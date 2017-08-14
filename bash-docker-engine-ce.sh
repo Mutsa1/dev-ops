fcd43557347aae17f6e11ce0d5dfa1f5d12039b3
JinnaBalu <jinna.balu@wisestep-inc.com>
2017-08-14 21:06:34 +0530

    changes related to commets of deploying docker engine CE

--- bash-docker-engine-ce.sh
+++ bash-docker-engine-ce.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+echo "Uninstall old versions \n"
+
 sudo apt-get remove docker docker-engine docker.io
 
+echo "Uninstall old versions Completed \n"
+
+echo "Install using the repository \n"
+
+echo "\n"
+
+echo " SET UP THE REPOSITORY \n"
+
+echo "1. Update the apt package index: \n"
+
 sudo apt-get update
+
+echo "2. Install packages to allow apt to use a repository over HTTPS: \n"
 
 sudo apt-get install \
     apt-transport-https \
@@ -10,21 +24,34 @@ sudo apt-get install \
     curl \
     software-properties-common
 
-
+echo "3. Add Docker’s official GPG key: \n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+echo "Verify that the key fingerprin \n "
 sudo apt-key fingerprint 0EBFCD88
+
+echo "set up the stable repository \n"
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
- sudo apt-get update
+echo "============================= INSTALL DOCKER CE ============================ \n\n"
 
- sudo apt-get install docker-ce
+echo "Update the apt package index: \n"
+sudo apt-get update
 
- sudo docker run hello-world
+echo "Install the latest version of Docker CE \n"
+sudo apt-get install docker-ce
 
- docker version
+echo "After the successful completion of the docker please check with \n\n\n"
+
+echo "DOKER VERSION \n\n"
+
+docker version
+
+echo "TO TEST TYPE : sudo docker run hello-world"
+
+
  
